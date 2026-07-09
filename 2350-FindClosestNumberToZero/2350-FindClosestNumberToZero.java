0cd9e3283e346ebee313d1c790e6e2dea3910b7e
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 23:08:54 -0700

    Create 2350-FindClosestNumberToZero

---
+++ 2350-FindClosestNumberToZero/2350-FindClosestNumberToZero.java
@@ -0,0 +1,21 @@
+// Last updated: 08/07/2026, 23:28:23
+class Solution {
+    public int findClosestNumber(int[] nums) {
+       int mindist=Integer.MAX_VALUE;
+       int index=-1;
+       for(int i=0;i<nums.length;i++){
+        int dist=Math.abs(nums[i])-0;
+        if(dist<mindist)
+       {
+        mindist=dist;
+        index=i;
+       }
+       else if(dist==mindist){
+        index=nums[i]>nums[index]?i:index;
+       }
+       }
+       return nums[index];
+
+       
+    }
+}
