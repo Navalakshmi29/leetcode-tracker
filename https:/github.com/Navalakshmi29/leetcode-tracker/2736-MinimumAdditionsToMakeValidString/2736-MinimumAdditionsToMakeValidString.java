f5eb941c9b25d093d134566ae626688a95a9aaed
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 21:21:41 -0700

    Create 2736-MinimumAdditionsToMakeValidString

---
+++ https:/github.com/Navalakshmi29/leetcode-tracker/2736-MinimumAdditionsToMakeValidString/2736-MinimumAdditionsToMakeValidString.java
@@ -0,0 +1,24 @@
+// Last updated: 08/07/2026, 21:41:11
+class Solution {
+    public int addMinimum(String word) {
+          int n=word.length(),i=0,res=0;
+          //int count =0;
+          while(i<n){
+            int count=0;
+            if( word.charAt(i)=='a'){
+               count++;
+               i++;
+            }
+            if(i<n && word.charAt(i)=='b'){
+                count++;
+                i++;
+            }
+            if(i<n && word.charAt(i)=='c'){
+                count++;
+                i++;
+            }
+            res+=3-count;
+          }  
+          return res;
+    }
+}
