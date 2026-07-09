89fbfb249696bc54ff3080eacf831c9346cc6d18
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 21:25:05 -0700

    Create 299-BullsAndCows

---
+++ 299-BullsAndCows/299-BullsAndCows.java
@@ -0,0 +1,31 @@
+// Last updated: 08/07/2026, 21:44:34
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+
+        int[] count = new int[10];
+
+        for (int i = 0; i < secret.length(); i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+
+            if (s == g) {
+                bulls++;
+            } else {
+                if (count[s - '0'] < 0) {
+                    cows++;
+                }
+
+                if (count[g - '0'] > 0) {
+                    cows++;
+                }
+
+                count[s - '0']++;
+                count[g - '0']--;
+            }
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
