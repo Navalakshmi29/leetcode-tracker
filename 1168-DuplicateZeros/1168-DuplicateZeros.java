9ad8a0f1d81d2d78636a82448c83647222967023
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 21:23:38 -0700

    Create 1168-DuplicateZeros

---
+++ 1168-DuplicateZeros/1168-DuplicateZeros.java
@@ -0,0 +1,21 @@
+// Last updated: 08/07/2026, 21:43:07
+class Solution {
+    public void duplicateZeros(int[] arr) {
+
+       int temp[]=new int[arr.length];
+       int j=0;
+       for(int i=0;i<arr.length && j<arr.length;i++){
+          temp[j]=arr[i];
+          j++;
+          if(arr[i]==0 && j<arr.length){
+            temp[j]=0;
+            j++;
+          }
+
+       }
+       for(int i=0;i<arr.length;i++){
+          arr[i]=temp[i];
+       }
+    }
+}
+   
