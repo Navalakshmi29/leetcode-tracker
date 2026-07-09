2b9cef11f830dae2b44cbcd570aa82bb57170c7c
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 23:08:00 -0700

    Create 3838-PathExistenceQueriesInAGraphI

---
+++ 3838-PathExistenceQueriesInAGraphI/3838-PathExistenceQueriesInAGraphI.java
@@ -0,0 +1,26 @@
+// Last updated: 08/07/2026, 23:27:29
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+
+        int[] group = new int[n];
+        int id = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                id++;
+            }
+            group[i] = id;
+        }
+
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            ans[i] = (group[u] == group[v]);
+        }
+
+        return ans;
+    }
+}
