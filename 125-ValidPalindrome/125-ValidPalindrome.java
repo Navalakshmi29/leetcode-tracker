4f4ef2f4a8dd1f6f5c8aefce2194d37a65cd3c3b
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 21:29:32 -0700

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,22 @@
+// Last updated: 08/07/2026, 21:49:01
+class Solution {
+    public boolean isPalindrome(String s) {
+       int l=0;
+       int r=s.length()-1;
+       while(l<r){
+        while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
+            l++;
+        }
+        while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
+            r--;
+        }
+       
+       if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+        return false;
+       }
+       l++;
+       r--;
+       }
+       return true;
+    }
+}
