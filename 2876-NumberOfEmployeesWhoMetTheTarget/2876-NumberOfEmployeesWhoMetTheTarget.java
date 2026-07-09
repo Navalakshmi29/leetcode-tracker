958ffdeab11982a59b3893cab8b1c56940243cb1
Navalakshmi <23cs064@drngpit.ac.in>
2026-07-08 23:08:36 -0700

    Create 2876-NumberOfEmployeesWhoMetTheTarget

---
+++ 2876-NumberOfEmployeesWhoMetTheTarget/2876-NumberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,14 @@
+// Last updated: 08/07/2026, 23:28:04
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int c=0;
+        for(int num:hours){
+            
+            if(num>=target){
+                c++;
+            }
+            
+        }
+        return c;
+    }
+}
